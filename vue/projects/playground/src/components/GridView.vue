24c90315601555a107fbce74b8263bcdbe62ea4f
TwitchBluelow <48391911+TwitchBluelow@users.noreply.github.com>
2024-05-27 08:35:48 +0200

    playground: init project

---
+++ vue/projects/playground/src/components/GridView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TestCard from './TestCard.vue'
+</script>
+
+<template>
+  <div class="grid gap-4 grid-cols-3 grid-rows-3">
+    <!-- ... -->
+    <TestCard />
+    <TestCard />
+    <TestCard />
+    <!-- ... -->
+    <TestCard />
+  </div>
+</template>
